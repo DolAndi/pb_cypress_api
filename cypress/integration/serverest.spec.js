7226adb4995aac3e2562b0637356c6bf31b41c67
EduardoRodrigues18 <eduardorodrigues1599@gmail.com>
2021-10-19 16:52:42 -0300

    commit com ajuda do codigo do kartabil

--- cypress/integration/serverest.spec.js
+++ cypress/integration/serverest.spec.js
@@ -1,16 +1,28 @@
 /// <reference types="cypress" />
 
-describe('testes na api sererest', () =>{ 
-    it('Deve trazer um usuário admin para login', ()=>{ 
-        cy.BuscarUserAdmin().then( usuario =>{
-            cy.wrap({email: usuario.email, password: usuario}).as('usuarioParaLogin')
+var bearer
+
+describe('Testes na api serverest', () => {
+    it('Deve trazer um usuário administrador para login', () => {
+        cy.buscarUsuarioAdmin().then( res => {
+            cy.wrap({email: res.email, password: res.password}).as('usuarioParaLogin')
+            // {"email": "fulano@qa.com", "password": "teste"}
         })
-        cy.get('@usuarioParaLogin').then(user =>{
-            cy.logar(user).then(res =>{
-                expect(res.status).to.be.equal(200)
-                expect(res.body).to.have.property('authorizaton')
+        cy.get('@usuarioParaLogin').then( user => {
+            cy.logar(user).then( res => {
+                expect(res.status).to.equal(200)
+                expect(res.body).to.have.property('authorization')
+                bearer = res.body.authorization
             })
         })
+    })
 
+    it('Deve cadastrar um novo carrinho com sucesso', () => {
+        cy.criarCarrinho(bearer)
     })
 })
+
+// Um cenário positivo e um negativo para os verbos e rotas: 
+// POST: /login => o caso negativo
+// POST: /usuarios
+// POST: /produtos
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,52 +1,41 @@
-// ***********************************************
 /// <reference types="cypress" />
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('BuscarUserAdmin',() => { 
+
+Cypress.Commands.add('buscarUsuarioAdmin', () => {
     cy.request({
         method: 'GET',
         url: `${Cypress.env('base_url')}/usuarios`,
-        failOnStatusCode: false//se receber um erro ele continua os testes
-    }).then(res =>{
+        failOnStatusCode: false //Se receber um 4xx ele não irá parar a automação
+    }).then( res => {
+
         expect(res.status).to.be.equal(200)
         expect(res.body).to.have.property('quantidade')
         expect(res.body.usuarios).to.be.a('array')
-        for(var i = 0; i < res.body.usuarios.length; i++){
-            if(res.body.usuarios[i].administrador == "true"){
+
+        for(var i = 0; i < res.body.usuarios.length; i++) {
+            if(res.body.usuarios[i].administrador === "true"){
                 return res.body.usuarios[i]
             }
-            console.log('lista>>>',res.body.usuarios[i]);
         }
     })
- })
+})
 
- Cypress.Commands.add('logar', usuarios =>{
-     return cy.request({
+Cypress.Commands.add('logar', usuario => {
+    return cy.request({
         method: 'POST',
         url: `${Cypress.env('base_url')}/login`,
         failOnStatusCode: true,
         body: usuario
-     })
- })
+    })
+})
+
+Cypress.Commands.add('cadastrarCarrinho', (bearer,produto) => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/carrinho`,
+        failOnStatusCode: true,
+        body: produto,
+        headers: {
+            Authorization: bearer.replace('bearer', '')
+        }
+    })
+})
