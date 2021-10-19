2020f096ea15175e8f57c6ca6e1c6f963dabd1d8
Leonardo Kartabil <leonardo.kartabil@compasso.com.br>
2021-10-19 16:04:23 -0300

    Adicionado exemplos de cypress nativo para API

---
+++ cypress/integration/serverest.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+var bearer
+
+describe('Testes na api serverest', () => {
+    it('Deve trazer um usuário administrador para login', () => {
+        cy.buscarUsuarioAdmin().then( res => {
+            cy.wrap({email: res.email, password: res.password}).as('usuarioParaLogin')
+            // {"email": "fulano@qa.com", "password": "teste"}
+        })
+        cy.get('@usuarioParaLogin').then( user => {
+            cy.logar(user).then( res => {
+                expect(res.status).to.equal(200)
+                expect(res.body).to.have.property('authorization')
+                bearer = res.body.authorization
+            })
+        })
+    })
+
+    it('Deve cadastrar um novo carrinho com sucesso', () => {
+        cy.criarCarrinho(bearer)
+    })
+})
+
+// Um cenário positivo e um negativo para os verbos e rotas: 
+// POST: /login => o caso negativo
+// POST: /usuarios
+// POST: /produtos
