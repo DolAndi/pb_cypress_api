8ca5466b3b945b17c6e9a0952a6044123e6bc261
Julio Cesar Spanholi <jcspanholi30@hotmail.com>
2021-10-19 16:45:33 -0300

    Adicionando código - MasterClass

---
+++ cypress/integration/serverest.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+var bearer
+
+describe('Testes na api serverest', () => {
+    it('Deve trazer um usuário administrador para login', () => {
+        cy.buscarUsuarioAdmin().then( res => {
+            cy.wrap({email: res.email, password: res.password}).as('usuarioParaLogin')
+            // {"email": "fulano@qa.com", "password": "teste"}
+        })
+        cy.get('@usuarioParaLogin').then( user => {
+            cy.logar(user).then( res => {
+                expect(res.status).to.equal(200)
+                expect(res.body).to.have.property('authorization')
+                bearer = res.body.authorization
+            })
+        })
+    })
+
+    it('Deve cadastrar um novo carrinho com sucesso', () => {
+        cy.criarCarrinho(bearer).then( user)
+    })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+Cypress.Commands.add('buscarUsuarioAdmin', () => {
+    cy.request({
+        method: 'GET',
+        url: `${Cypress.env('base_url')}/usuarios`,
+        failOnStatusCode: false //Se receber um 4xx ele não irá parar a automação
+    }).then( res => {
+
+        expect(res.status).to.be.equal(200)
+        expect(res.body).to.have.property('quantidade')
+        expect(res.body.usuarios).to.be.a('array')
+
+        for(var i = 0; i < res.body.usuarios.length; i++) {
+            if(res.body.usuarios[i].administrador === "true"){
+                return res.body.usuarios[i]
+            }
+        }
+    })
+})
+
+Cypress.Commands.add('logar', usuario => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/login`,
+        failOnStatusCode: true,
+        body: usuario
+    })
+})
+
+Cypress.Commands.add('cadastrarCarrinho', (bearer,produto) => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/carrinho`,
+        failOnStatusCode: true,
+        body: produto,
+        headers: {
+            Authorization: bearer.replace('bearer', '')
+        }
+    })
+})
