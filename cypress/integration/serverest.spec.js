d77a048ed4f275ca995e263a6026146be5e6de49
vih2021 <vitoria5646@gmail.com>
2021-10-19 23:58:37 -0300

    quase pronto :D

--- cypress/integration/serverest.spec.js
+++ cypress/integration/serverest.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+let bearer = ""
+
 
 describe('testes de api serverest', () => { 
     it('Deve trazer um usuário administrador para login', () => { 
@@ -7,14 +9,56 @@ describe('testes de api serverest', () => {
             cy.wrap({email: usuario.email, password: usuario.password}).as("usuarioParaLogin")
             //{email: "fulano@qa.com", "password": "teste"}   
         })
-
-
+    
         cy.get('@usuarioParaLogin').then(user => {
             cy.logar(user).then( res => {
                 expect(res.status).to.be.equal(200)
                 expect(res.body).to.have.property("authorization")
-            })
+                bearer = res.body.authorization
+         })
+    })
+})
 
+    it('Não deve realizar o login caso o email ou senha sejam inválidos', () => { 
+        cy.loginInvalido().then( res => {
+            expect(res.status).to.be.equal(401) // na documentação diz que deveria ser 400...
+            expect(res.body).to.not.have.property("authorization")
+            expect(res.body.message).to.be.equal("Email e/ou senha inválidos")
+        })
+    })
+
+    it('Deve cadastrar um novo produto', () => {
+        cy.cadastrarProduto(bearer).then( res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property("message")
+            expect(res.body).to.have.property("_id")         
+        })
+    })
+
+    it('Não deve cadastrar um produto com o mesmo nome', () => {
+        cy.cadastrarProduto(bearer).then( res => {
+            expect(res.status).to.be.equal(400)
+            expect(res.body).to.not.have.property("_id")
+            expect(res.body.message).to.be.equal("Já existe produto com esse nome")         
+        })
+    })
+
+    it('Deve cadastrar um novo usuario', () => {
+        cy.cadastrarUsuario().then( res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property("message")
+            expect(res.body).to.have.property("_id")
+            expect(res.body.message).to.be.equal("Cadastro realizado com sucesso")         
+        })
+    })
+
+    it('Não deve cadastrar um usuario ja existente', () => {
+        cy.cadastrarUsuario().then( res => {
+            expect(res.status).to.be.equal(400)
+            expect(res.body).to.not.have.property("_id")
+            expect(res.body.message).to.be.equal("Este email já está sendo usado")         
         })
     })
 })
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,11 +7,12 @@ Cypress.Commands.add('buscarUsuarioAdmin', () => {
         failOnStatusCode: false // 4xx ele não irá parar a automação
     }).then( res => { 
 
-        expect(res.status).to.be.equal(200)
-        expect(res.body).to.have.property('quantidade')
-        expect(res.body.usuarios).to.be.a('array')
+            expect(res.status).to.be.equal(200)
+            expect(res.body).to.have.property('quantidade')
+            expect(res.body.usuarios).to.be.a('array')
 
         //let listaUsuarios = res.body.usuarios
+
         for(var i = 0; i < res.body.usuarios.length; i++) {
             if(res.body.usuarios[i].administrador === 'true' ){
                 return res.body.usuarios[i]
@@ -19,11 +20,53 @@ Cypress.Commands.add('buscarUsuarioAdmin', () => {
         }
     })
 })
+
+Cypress.Commands.add('loginInvalido', () => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/login`,
+        failOnStatusCode: false,
+        body:{
+            "email": "aleatorio@qa.com",
+            "password": "aleatorio"
+        }
+    })
+})
+
 Cypress.Commands.add('logar', usuario => {
     return cy.request({
         method: 'POST',
         url: `${Cypress.env('base_url')}/login`,
         failOnStatusCode: false,
-        body: usuario
+        body: usuario,
+    })
+})
+Cypress.Commands.add('cadastrarProduto', (bearer) => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/produtos`,
+        failOnStatusCode: false,
+        body:{
+            "nome": "batata colorida",
+            "preco": 445,
+            "descricao": "unicorn",
+            "quantidade": 3
+        },
+        headers:{
+            Authorization : bearer
+        }
+    })
+})
+Cypress.Commands.add('cadastrarUsuario', () => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/usuarios`,
+        failOnStatusCode: false,
+        body:{
+            "nome": "ninguém",
+            "email": "ninguem@qa.com.br",
+            "password": "ninguem",
+            "administrador": "true"
+        }
     })
 })
