0aa92c5e4e08e71bc0dcd817f68c0ff139160e05
Kaline Maciel_ <kaline.santos_BOLS@compasso.com.br>
2021-10-20 10:45:14 -0300

    Atividades Cypress

--- cypress/integration/servrest.spec.js
+++ cypress/integration/servrest.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+var bearer 
 
 describe('Teste na api serverest', () => {
     it('Deve trazer um usuário administrador para login', () => {
@@ -13,4 +14,44 @@ describe('Teste na api serverest', () => {
             })
         })
     })
-})
+    it('Não deve realizar login', () => {
+        cy.naoLogar().then( res => {
+            expect(res.status).to.be.equal(400)
+            expect(res.body).to.have.property('message').to.be.equal('Email e/ou senha inválidos')
+        })
+    })
+    it('Deve cadastrar usuario' , () => {
+        cy.cadastrarUsuario().then( res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('message').to.be.equal('Cadastro realizado com sucesso')
+            expect(res.body).to.have.property('_id')
+        })
+    })
+    it('Não deve cadastrar usuario' , () => {
+        cy.naoCadastrarUsuario().then( res => {
+            expect(res.status).to.be.equal(400)
+            expect(res.body).to.have.property('message').to.be.equal('Este email já está sendo usado')
+        })
+    })
+
+    it('Deve cadastrar produto' , () => {
+        cy.cadastrarProduto(bearer).then( res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('message').to.be.equal('Cadastro realizado com sucesso')
+            expect(res.body.to.have.property('_id'))
+        })
+    
+    })
+
+    it('Não deve cadastrar produto' , () => {
+        cy.naoCadastrarProduto(bearer).then( res => {
+            expect(res.status).to.be.equal(400)
+            expect(res.body).to.have.property('message').to.be.equal('Já existe produto com esse nome')
+        })
+    
+    })
+
+
+}) 
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,3 +25,77 @@ Cypress.Commands.add('buscarUsuarioAdmin', () => {
         body: usuario
     })
  })
+ 
+ Cypress.Commands.add('naoLogar', () => {
+    return cy.request({
+            method: 'POST',
+            url: `${Cypress.env('base_url')}/login`,
+            failOnStatusCode: false,
+            body: {
+                "email": "piriri@qa.com",
+                "password": "teste"
+              }
+    })
+ })
+
+ Cypress.Commands.add('cadastrarUsuario', () => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/usuarios`,
+        failOnStatusCode: false,
+        body: {
+            "nome": "Fulanito",
+            "email": "fulanitobe@qa.com.br",
+            "password": "teste",
+            "administrador": "true"
+          }
+    })
+})
+
+Cypress.Commands.add('naoCadastrarUsuario', () => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/usuarios`,
+        failOnStatusCode: false,
+        body: {
+            "nome": "Fulanito",
+            "email": "fulanitog@qa.com.br",
+            "password": "teste",
+            "administrador": "true"
+          }
+    })
+})
+
+Cypress.Commands.add('cadastrarProduto', (bearer) => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/produtos`,
+        failOnStatusCode: false,
+        body: {
+            "nome": "Teenage Dream Deluxe Edition",
+            "preco": 60,
+            "descricao": "CD",
+            "quantidade": 480
+          },
+          headers: {
+              Authorization: bearer
+            }
+    })
+})
+
+Cypress.Commands.add('naoCadastrarProduto', (bearer) => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/produtos`,
+        failOnStatusCode: false,
+        body: {
+            "nome": "Teenage Dream Deluxe Edition",
+            "preco": 60,
+            "descricao": "CD",
+            "quantidade": 480
+          },
+          headers: {
+              Authorization: bearer
+            }
+    })
+})
