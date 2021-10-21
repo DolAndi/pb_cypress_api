fae6f493d9037c955a2d550571b892c08bf11465
Ana Haubert <anacarolinna.ach@gmail.com>
2021-10-21 02:02:50 -0300

    atividades

---
+++ cypress/support/commands.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('buscarUsuarioAdmin', (email, password) => {
+    cy.request({
+        method: 'GET',
+        url: `${Cypress.env('base_url')}/usuarios`,
+        failOnStatusCode: false
+    }).then((res) => {
+
+        expect(res.status).to.be.equal(200)
+        expect(res.body).to.have.property('quantidade')
+        expect(res.body.usuarios).to.be.a('array')
+
+        for(var i = 0; i < res.body.usuarios.length; i++) {
+            if(res.body.usuarios[i].administrador ===  "true"){
+                return res.body.usuarios[i]
+            }
+            console.log("Lista >>", res.body.usuarios[i])
+        }
+    })
+
+})
+
+Cypress.Commands.add('logar', usuario => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/login`,
+        failOnStatusCode: true,
+        body: usuario
+    })
+})
+
+Cypress.Commands.add('cadastarCarrinho', (bearer, produto) => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('base_url')}/carrinho`,
+        failOnStatusCode: true,
+        body: produto,
+        headers: {
+            Authorizarion: bearer.replace('bearer', '')
+        }
+    })
+})
